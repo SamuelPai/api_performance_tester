5ae4fb790a5bcaef705afa7314b033278a5d1b41
Samuel Pai <samuelpai16@gmail.com>
2022-10-12 21:32:05 -0700

    added program file to make concurrent requests to an endpoint

---
+++ program.py
@@ -0,0 +1,24 @@
+import requests
+import concurrent
+from concurrent.futures import ThreadPoolExecutor
+import time
+characters = range(1, 100)
+base_url = 'https://rickandmortyapi.com/api/character'
+threads = 20
+
+def get_character_info(character):
+    r = requests.get(f'{base_url}/{character}')
+    return r.json()
+
+with ThreadPoolExecutor(max_workers=threads) as executor:
+    start_time = time.time()
+    future_to_url = {executor.submit(get_character_info, char)
+    for char in characters}
+    for future in concurrent.futures.as_completed(future_to_url):
+        try:
+            data = future.result()
+            print(data)
+        except Exception as e:
+            print('Looks like something went wrong:', e)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    # 0.9544878005981445
